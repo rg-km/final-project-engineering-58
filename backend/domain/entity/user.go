6e5384e32975aaa5faca3b3384eb3d586e9484de
syaddad <sayaddad@qazzkop.localdomain>
2022-06-15 15:28:40 +0700

    commit

--- backend/domain/entity/user.go
+++ backend/domain/entity/user.go
@@ -27,6 +27,11 @@ type UserDto struct {
 }
 
 func NewUser(payload UserDto) *User {
+	// location := time.FixedZone("UTC", 0)
+	// now := time.Now()
+	// utc := now.In(location)
+	// utcFormat := utc.Format(time.RFC3339)
+	//corrFormat, _ := time.Parse(time.RFC3339, utcFormat)
 	create := User{
 		ID:        common.NewID(),
 		Name:      payload.Name,
--- backend/internal/repository/models/user.go
+++ backend/internal/repository/models/user.go
@@ -3,13 +3,13 @@ package models
 import "time"
 
 type User struct {
-	ID             		string    `dbq:"id"`
-	Name           		string    `dbq:"name"`
-	Email          		string    `dbq:"email"`
-	Password	 		string 	  `dbq:"password"`
-	Role          		string    `dbq:"role"`
-	CreatedAt      		time.Time `dbq:"created_at"`
-	UpdatedAt      		time.Time `dbq:"updated_at"`
+	ID        string    `dbq:"id"`
+	Name      string    `dbq:"name"`
+	Email     string    `dbq:"email"`
+	Password  string    `dbq:"password"`
+	Role      string    `dbq:"role"`
+	CreatedAt time.Time `dbq:"created_at"`
+	UpdatedAt time.Time `dbq:"updated_at"`
 }
 
 func (User) TableName() string {

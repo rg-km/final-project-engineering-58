cf81b92f3a53697825046344ec3b2dbd71ad2018
Angga Kusuma <manggakusuma810@gmail.com>
2022-06-17 21:56:34 +0700

    fix header and add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
-
+import Footer from "./components/Footer";
 const App = () =>{
     return (
         <div>
             <Header />
+            <Footer />
         </div>
     )
 }

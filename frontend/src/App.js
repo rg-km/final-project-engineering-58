d440577a8ae7169f2ef2f3b8164946d6d377b11b
Angga Kusuma <manggakusuma810@gmail.com>
2022-06-22 22:53:58 +0700

    fix login add register and dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
+import Dashboard from "./components/Dashboard";
 
 const App = () =>{
     return (
@@ -9,6 +11,8 @@ const App = () =>{
         <Routes>
             <Route index element={<LandingPage />} />
             <Route path="/login" element={<LoginForm />} />
+            <Route path="/sign-up" element={<RegisterForm />} />
+            <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
         </>
     )
